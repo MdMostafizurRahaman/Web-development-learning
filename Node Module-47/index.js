e039eefece37e669070abf7c870bfef7723fb5e0
Mostafizur <bsse1320@iit.du.ac.bd>
2024-10-06 23:48:49 +0600

    47-4 install nodemon, api return object, create multiple api

--- Node Module-47/index.js
+++ Node Module-47/index.js
@@ -3,7 +3,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) =>{
-    res.send("Thank U")
+    const fruits ={
+        product: "ada",
+        price: 123
+    }
+    res.send(fruits)
 })
 
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruits: 'banana', quantity: 1000, price: 10000})
+})
 app.listen(3000, () => console.log("Listening to port 3000"))
