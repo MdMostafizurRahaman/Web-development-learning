9dafd5db92cd3a106c88eaa4b07425947b82a351
Mostafizur <bsse1320@iit.du.ac.bd>
2024-10-07 00:28:38 +0600

    47-6 use fetch to load data from server, middleware, handle cors

--- Node Module-47/index.js
+++ Node Module-47/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
+
 
 app.get('/', (req, res) =>{
     const fruits ={
@@ -14,11 +18,11 @@ app.get('/fruits/banana', (req, res) => {
     res.send({fruits: 'banana', quantity: 1000, price: 10000})
 })
 
-const users = ['Rakib', 'sakib', 'eshan', 'zim', 'Asif']
+const users = ['rakib', 'sakib', 'eshan', 'zim', 'Asif'];
 
 app.get('/users/:id', (req, res) =>{
-    const userId = req.params.id
-    const name = users[userId]
-    res.send(name)
+    const id = req.params.id
+    const name = users[id]
+    res.send({id, name})
 })
 app.listen(3000, () => console.log("Listening to port 3000"))
