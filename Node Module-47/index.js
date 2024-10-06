599e9f1482eaaa94c37ca432e3501f49f45d9c44
Mostafizur <bsse1320@iit.du.ac.bd>
2024-10-07 01:44:01 +0600

    Post request

--- Node Module-47/index.js
+++ Node Module-47/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) =>{
@@ -25,4 +26,14 @@ app.get('/users/:id', (req, res) =>{
     const name = users[id]
     res.send({id, name})
 })
+
+
+//post
+app.post('/addUser', (req, res) => {
+    const user = req.body
+    user.id = 55
+    res.send(user)
+})
+
+
 app.listen(3000, () => console.log("Listening to port 3000"))
