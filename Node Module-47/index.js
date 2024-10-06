65b24d8a02e658e048438529149b1be811900d6f
Mostafizur <bsse1320@iit.du.ac.bd>
2024-10-07 00:03:15 +0600

    47-5 dynamic api, api parameter, access params, access query

--- Node Module-47/index.js
+++ Node Module-47/index.js
@@ -13,4 +13,12 @@ app.get('/', (req, res) =>{
 app.get('/fruits/banana', (req, res) => {
     res.send({fruits: 'banana', quantity: 1000, price: 10000})
 })
+
+const users = ['Rakib', 'sakib', 'eshan', 'zim', 'Asif']
+
+app.get('/users/:id', (req, res) =>{
+    const userId = req.params.id
+    const name = users[userId]
+    res.send(name)
+})
 app.listen(3000, () => console.log("Listening to port 3000"))
